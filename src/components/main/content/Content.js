bc6908c8a11612dbb4cbd58c088c5e39672e26f8
khanmosajjid <mosajjid.khan@gmail.com>
2020-07-08 23:24:59 +0530

    countdown functionality

--- src/components/main/content/Content.js
+++ src/components/main/content/Content.js
@@ -1,11 +1,12 @@
 import React from "react";
-import './Content.css'
+import './Content.css';
+import Counter from '../counter/Counter'
 const Content = () => {
   return (
     <div className="content-container">
       <h1>ETHER CHAIN</h1>
       <div className="calc">
-        <span className="hightlight-value">00:00:00</span>
+        <Counter></Counter>
         <span className="unit">/---ETH</span>
       </div>
       <p>A FULLY DECENTRALISED COMMUNITY FUND,</p>
---
+++ src/components/main/counter/Counter.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+
+export default class Counter extends Component{
+    // const date=new Date();
+    state={
+        
+        hour:23-new Date().getHours(),
+        min:60-new Date().getMinutes(),
+        sec:60-new Date().getSeconds(),
+    }
+
+     
+      
+      componentDidMount(){
+          this.myInterval=setInterval(()=>{
+            const{hour,min,sec}=this.state
+            if(sec>0){
+                this.setState(({sec})=>({
+                 sec:sec-1,
+                }))
+                console.log('sec calling');
+            }
+           
+           
+            else if(sec===0){
+                if(min===0){
+                    if(hour===0){
+                        this.setState(({hour})=>({
+                            hour:24,
+                            min:0,
+                            sec:0,
+                        }))
+                    }
+                    else{
+                        this.setState(({hour})=>({
+                            hour:hour-1,
+                            min:59,
+                            sec:59
+                        }))
+                        console.log("hour calling");
+
+                    }
+                  
+                }
+                else{
+                    this.setState(({min})=>({
+                        min:min-1,
+                        sec:59,
+                    }))
+                    console.log("min calling");
+                }
+               
+
+            }
+            
+        
+          },1000)
+          
+      }
+      componentWillUnmount(){
+          clearInterval(this.myInterval)
+      }
+     
+      
+     render(){
+        //  this.timer();
+        //  this.componentWillUnmount();
+        //  this.setState({hour:h});
+    // const date=new Date();
+     
+       const {hour,min,sec}=this.state;
+
+         return(
+         <h1>{hour}:{min}:{sec}</h1>
+         )
+     }
+}
